8a9f4d50c6721d8c5aec2c73aa160fd6c9f05c07
James Yamazaki <james.yamazaki@sumsub.com>
2025-03-30 13:49:42 +0500

    first commit

---
+++ lua/lovelace/init.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+-- Load color palette
+local palette = require("lovelace.palette")
+
+-- Configure highlights
+function M.setup()
+  vim.opt.termguicolors = true
+  vim.g.colors_name = "lovelace"
+
+  local highlights = {
+    -- Base UI
+    Normal = { fg = palette.fg, bg = palette.bg },
+    NormalFloat = { fg = palette.fg, bg = palette.bg },
+    EndOfBuffer = { fg = palette.bg }, -- Hide ~ tildes
+    LineNr = { fg = palette.gray1 },
+    CursorLineNr = { fg = palette.fg, bold = true },
+    VertSplit = { fg = palette.gray1 },
+    SignColumn = { bg = palette.bg },
+
+    -- Syntax
+    Comment = { fg = palette.gray1 },
+    String = { fg = palette.orange },
+    Number = { fg = palette.purple },
+    Boolean = { fg = palette.purple },
+    Function = { fg = palette.blue },
+    Identifier = { fg = palette.cyan },
+    Keyword = { fg = palette.red },
+    Type = { fg = palette.aqua },
+    Operator = { fg = palette.fg },
+
+    -- Diagnostics
+    DiagnosticError = { fg = palette.red },
+    DiagnosticWarn = { fg = palette.orange },
+    DiagnosticInfo = { fg = palette.blue },
+    DiagnosticHint = { fg = palette.cyan },
+
+    -- Git
+    DiffAdd = { fg = palette.green },
+    DiffDelete = { fg = palette.red },
+    DiffChange = { fg = palette.yellow },
+    DiffText = { fg = palette.orange },
+
+    -- Plugins
+    TelescopeBorder = { fg = palette.gray1 },
+    LspFloatWinNormal = { bg = palette.bg },
+  }
+
+  -- Apply highlights
+  for group, settings in pairs(highlights) do
+    vim.api.nvim_set_hl(0, group, settings)
+  end
+
+  -- Terminal colors
+  for i, color in ipairs(palette.terminal_colors) do
+    vim.g["terminal_color_" .. (i-1)] = color
+  end
+end
+
+return M
---
+++ lua/lovelace/palette.lua
@@ -0,0 +1,36 @@
+local M = {
+  bg = "#1d1f28",
+  fg = "#fdfdfd",
+  gray1 = "#414458",
+  gray2 = "#bebec1",
+  red = "#f37f97",
+  orange = "#f2a272",
+  yellow = "#ff8037",
+  green = "#18e3c8",
+  cyan = "#5adecd",
+  blue = "#8897f4",
+  purple = "#c574dd",
+  pink = "#ff4971",
+  aqua = "#79e6f3",
+
+  terminal_colors = {
+    "#282a36",   -- 0
+    "#f37f97",   -- 1
+    "#5adecd",   -- 2
+    "#f2a272",   -- 3
+    "#8897f4",   -- 4
+    "#c574dd",   -- 5
+    "#79e6f3",   -- 6
+    "#fdfdfd",   -- 7
+    "#414458",   -- 8
+    "#ff4971",   -- 9
+    "#18e3c8",   -- 10
+    "#ff8037",   -- 11
+    "#556fff",   -- 12
+    "#b043d1",   -- 13
+    "#3fdcee",   -- 14
+    "#bebec1",   -- 15
+  }
+}
+
+return M
